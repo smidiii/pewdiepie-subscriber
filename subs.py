0190cd01661ec298b5b767afdbe2257da4190ea9
Smidi <40966301+smidiii@users.noreply.github.com>
2019-02-22 23:15:11 +0100

    Add files via upload

---
+++ subs.py
@@ -0,0 +1,23 @@
+import urllib.request
+import json
+import time
+
+key = "/*get the api key by google*/"
+
+datapewds = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+"pewdiepie"+"&key="+key).read()
+subspewds = int(json.loads(datapewds.decode('utf-8'))["items"][0]["statistics"]["subscriberCount"])
+
+datatseries = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+"tseries"+"&key="+key).read()
+substseries = int(json.loads(datatseries.decode('utf-8'))["items"][0]["statistics"]["subscriberCount"])
+
+diff = subspewds-substseries
+
+#print("Pewdiepie: " + "{:,d}".format(subspewds))
+#print("T-Series: " + "{:,d}".format(substseries))
+#print("Difference: " + "{:,d}".format(diff))
+
+f = open('pewdiepie_vs_tseries.csv', 'a+')
+date = time.strftime("%d.%m.%Y")
+time = time.strftime("%H:%M:%S")
+f.write(date + ';' +  time  + ';' + str(subspewds) + ';'  + str(substseries) + ';' + str(diff) + ';' + '\n')
+f.close
